441aef5b473e78d76028465f4816c9e2f1c5e2e2
mentama <mentama1408@gmail.com>
2025-01-30 01:00:00 +0900

    サインアップ作成

--- ChatApp/models.py
+++ ChatApp/models.py
@@ -1,9 +1,42 @@
 import pymysql
 from flask import abort
-from DB import DB # type: ignore
+from util.DB import DB # type: ignore
 
-# データベース接続クラス
+db_pool = DB.init_db_pool()
+
+#ユーザークラス
 class User:
+    @classmethod
+    def create(cls, user_name, email, password):
+        conn = db_pool.get_conn()
+        try:
+            with conn.cursor() as cur:
+                sql = "INSERT INTO users (user_name, email, password, is_admin, delete_flag) VALUES (%s, %s, %s, 0, 0);"
+                cur.execute(sql, (user_name, email, password,)) # type: ignore
+                conn.commit()
+        except pymysql.Error as e:
+            print(f'エラーが発生しています：{e}')
+            abort(500)
+        finally:
+            db_pool.release(conn)
+
+
+    @classmethod
+    def find_by_email(cls, email):
+        conn = db_pool.get_conn()
+        try:
+                with conn.cursor() as cur:
+                     sql = "SELECT * FROM users WHERE email=%s;"
+                     cur.execute(sql, (email,))
+                     user = cur.fetchone()
+                return user
+        except pymysql.Error as e:
+             print(f'エラーが発生しています：{e}')
+             abort(500)
+        finally:
+             db_pool.release(conn)
+
+
     @staticmethod
     def getUser(email):
         """
