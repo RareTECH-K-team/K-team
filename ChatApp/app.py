0092f323a2e9b2499c7c358f250af0afc44009b5
yoru@49 <raretech49yoru@gmail.com>
2025-02-23 22:02:21 +0900

    ログアウト処理追加場所修正

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -119,13 +119,6 @@ def login_process():
         return redirect(url_for('access_denied'))
 
 
-# ログアウト処理
-@app.route('/logout')
-def logout():
-    session.clear()
-    return redirect(url_for('login_view'))
-
-
 # 管理者ダッシュボード
 @app.route('/admin_dashboard', methods=['GET'])
 def admin_dashboard_view():
@@ -137,6 +130,14 @@ def admin_dashboard_view():
         all_users.reverse()
         print(all_users)
         return render_template('auth/admin_dashboard.html', users = all_users)
+
+
+# ログアウト処理
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login_view'))
+
 
 
 # チャンネル画面（一般ユーザー_work）
